d7bf8162b7df7d40bb68fea5f8e71d2cec7047d3
Tony Eltgroth <tonyelt8@gmail.com>
2018-12-26 19:32:21 -0600

    Updated files with spring initializer

---
+++ src/main/java/edu/playground/flyingclub/FlyingclubApplication.java
@@ -0,0 +1,14 @@
+package edu.playground.flyingclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyingclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyingclubApplication.class, args);
+	}
+
+}
+
